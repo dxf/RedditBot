27d36b91afcc7af5bc6983c24cc67da74c8f3f79
Dafydd <me@daf.lol>
2018-08-29 19:32:28 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -42,9 +42,8 @@ async def on_message(message):
                         while True:
                             response = http.request('GET',finalurl)
                             thedata = response.data
-                            parsedjson = json.loads(thedata)
-                            decode = parsedjson.decode('utf-8')
-                            img = encode[0]['data']['children'][0]['data']['url']
+                            parsedjson = json.loads(thedata).decode()
+                            img = parsedjson[0]['data']['children'][0]['data']['url']
                             if img.endswith('.png') or img.endswith('.jpg') or img.endswith('.gif'):
                                 await client.send_message(message.channel, img)
                                 return
